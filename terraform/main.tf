9f29a43b8b7c39d7fb17fe2662484667cc85ecab
Subin Silwal <silwalsubin@gmail.com>
2024-06-16 16:50:03 -0500

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -148,6 +148,10 @@ resource "tls_private_key" "my_key" {
   }
 }
 
+data "aws_key_pairs" "all" {
+  
+}
+
 resource "aws_key_pair" "my_key_pair" {
   key_name   = "my_key_pair"
   public_key = tls_private_key.my_key.public_key_openssh
