aed037fa33e3e64e63e35b12cc8e59c290e5f5f3
Subin Silwal <silwalsubin@gmail.com>
2024-06-16 13:35:44 -0500

    automation fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,7 +49,7 @@ resource "aws_subnet" "my_subnet" {
 }
 
 # Step 3: Check for Existing Internet Gateway and Create if it Doesn't Exist
-data "aws_internet_gateways" "all_igws" {
+data "aws_internet_gateway" "all_igws" {
   filter {
     name   = "tag:Name"
     values = ["MyInternetGateway"]  # Replace with your Internet Gateway name
