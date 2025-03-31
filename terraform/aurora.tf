8969d2d0839904d56d217161bbc924550b7e2401
Subin Silwal <silwalsubin@gmail.com>
2025-03-30 21:37:53 -0500

    test test again

---
+++ terraform/aurora.tf
@@ -0,0 +1,12 @@
+resource "aws_rds_cluster" "aurora" {
+  cluster_identifier      = "aurora-postgres-cluster"
+  engine                 = "aurora-postgresql"
+  engine_version         = "15.2"
+  database_name          = "mydatabase"
+  master_username        = "admin"
+  master_password        = "your-secure-password"
+  skip_final_snapshot    = true
+  backup_retention_period = 7
+  vpc_security_group_ids = [aws_security_group.aurora_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.aurora_subnet_group.name
+}
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,34 +2,30 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+resource "aws_subnet" "subnet_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
+resource "aws_subnet" "subnet_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
 }
 
-resource "aws_security_group" "lambda_sg" {
+resource "aws_db_subnet_group" "aurora_subnet_group" {
+  name       = "aurora-subnet-group"
+  subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+}
+
+resource "aws_security_group" "aurora_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.public_subnet.id
 }
