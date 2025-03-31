363e4199aa402de1dfa37b3e039651c54033770b
Subin Silwal <silwalsubin@gmail.com>
2025-03-30 20:58:49 -0500

    restart over

--- terraform/variables.tf
+++
@@ -1,8 +0,0 @@
-# File: terraform/variables.tf
-
-# Variable for environment
-variable "environment" {
-  description = "The deployment environment (e.g., dev, staging, prod)"
-  type        = string
-  default     = "dev" # Optional default value
-}
